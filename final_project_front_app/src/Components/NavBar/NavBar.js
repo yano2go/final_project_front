3a31bfb970efef5cf97f29a27cee8a9c43c03827
Alex Yanovich <alexyanovich@Alex.local>
2020-10-04 14:48:59 -0400

    mobile responsive navbar

--- final_project_front_app/src/Components/NavBar/NavBar.js
+++ final_project_front_app/src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import { Nav } from "react-bootstrap";
+import { Nav, NavDropdown } from "react-bootstrap";
 
 export default function NavBar(props) {
   console.log(props);
@@ -8,6 +8,15 @@ export default function NavBar(props) {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="blue">
+        <NavDropdown>
+          <NavDropdown.Item href="/cooking">cooking</NavDropdown.Item>
+          <NavDropdown.Item href="/diy">diy</NavDropdown.Item>
+          <NavDropdown.Item href="/programming">programming</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/artsandcrafts">
+            arts and crafts
+          </NavDropdown.Item>
+        </NavDropdown>
         <Navbar.Brand href="/">Learning in Gifs</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -19,6 +28,7 @@ export default function NavBar(props) {
             <Nav.Link href="/random">Random</Nav.Link>
             <Nav.Link href="/gifsearch">search for gifs!</Nav.Link>
           </Nav>
+
           <Nav>
             {props.isLoggedIn ? (
               <Nav.Link href="/profilepage">{username}</Nav.Link>
